6000498e287e0beac5d51a75966af69a15c6340d
Dushime Paulette <p.dushime12@gmail.com>
2024-10-12 11:50:14 +0200

    Added a current location

--- script.js
+++ script.js
@@ -22,6 +22,9 @@ function UpdateTime() {
 function Updatecity(event) {
   //here it is bcz the timezones are already set in the value attribute in the html
   let cityTimezones = event.target.value;
+  if (cityTimezones === "current-location") {
+    cityTimezones = moment.tz.guess();
+  }
   let cityName = cityTimezones.replace("_", " ").split("/")[1];
   let citytime = moment().tz(cityTimezones);
   let citiesId = document.querySelector("#cities");
@@ -38,5 +41,6 @@ function Updatecity(event) {
 }
 UpdateTime();
 setInterval(UpdateTime, 1000);
+
 let selectCity = document.querySelector("#city");
 selectCity.addEventListener("change", Updatecity);
